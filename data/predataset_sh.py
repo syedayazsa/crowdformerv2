198ca1333123e576e5ceff6a496b5514772eba2e
Syed Ayaz <51998832+syedayazsa@users.noreply.github.com>
2022-06-06 19:57:45 +0530

    Update predataset_sh.py

--- data/predataset_sh.py
+++ data/predataset_sh.py
@@ -11,7 +11,7 @@ from scipy.ndimage.filters import gaussian_filter
 import random
 
 '''set your data path'''
-root = '/home/dkliang/projects/synchronous/dataset/ShanghaiTech/'
+root = '/content/datasets/ShanghaiTech'
 
 part_A_train = os.path.join(root, 'part_A_final/train_data', 'images')
 part_A_test = os.path.join(root, 'part_A_final/test_data', 'images')
